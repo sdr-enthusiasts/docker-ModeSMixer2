49353f7ae7991f566ff1eca7f76aeac079eec0a2
Mike <mike.nye@gmail.com>
2020-08-04 20:52:48 +0800

    Tests

--- rootfs/cmd.sh
+++ rootfs/cmd.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/with-contenv bash
 #shellcheck shell=bash
 
-echo 'command line args:' $@ \
-    2>&1 | awk -W Interactive '{print "[dmtcp_coordinator] " $0}'
+echo 'command line args:' $@ 
+echo ""
+set
+echo ""
+
+
+# ENTRYPOINT [ "/usr/local/bin/dmtcp_launch" ]
+
+# CMD [ "--no-coordinator", "--no-gzip", "--ckptdir", "/data", "--modify-env", "/usr/local/bin/modesmixer2" ]
+
+#2>&1 | awk -W Interactive '{print "[cmd] " $0}'
