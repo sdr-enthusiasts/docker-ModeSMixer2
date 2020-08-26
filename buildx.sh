463dd385c5f1c3c19ab0e66310996f9c44163eb7
Mike <mike@narx.net>
2020-08-26 16:39:23 +0800

    Build platform-specific images & handle platform version differences

--- buildx.sh
+++ buildx.sh
@@ -2,11 +2,56 @@
 
 REPO=mikenye
 IMAGE=modesmixer2
-PLATFORMS="linux/386,linux/amd64,linux/arm/v7,linux/arm64"
+LINUX_PLATFORMS=('386' 'amd64' 'arm/v7' 'arm64')
+
+TMPPLATFORMS=""
+for p in "${LINUX_PLATFORMS[@]}"; do
+  TMPPLATFORMS+="linux/$p,"
+done
+PLATFORMS=${TMPPLATFORMS::-1}
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
+
+# Build & push latest platform specific
+for p in "${LINUX_PLATFORMS[@]}"; do
+  sanitised_p="$(echo $p | tr -d "/")"
+  docker buildx build -t ${REPO}/${IMAGE}:latest_${sanitised_p} --compress --push --platform "linux/$p" .
+done
+
+# Build & push version specific platform specific
+PLATFORM_VERSIONS=()
+for p in "${LINUX_PLATFORMS[@]}"; do
+  sanitised_p="$(echo $p | tr -d "/")"
+  case "$p" in
+  386 | amd64)
+    CONTEXT="x86_64"
+    ;;
+  arm/v7)
+    CONTEXT="arm32v7"
+    ;;
+  arm64)
+    CONTEXT="arm64"
+  *)
+    echo "ERROR: unknown context"
+    exit 1
+    ;;
+  esac
+  docker --context="$CONTEXT" pull ${REPO}/${IMAGE}:latest_${sanitised_p}
+  VERSION=$(docker --context="$CONTEXT" run --rm --entrypoint cat ${REPO}/${IMAGE}:latest_${sanitised_p} /VERSIONS | grep -i modesmixer2 | cut -d ' ' -f 2 | tr -d ' ')
+  PLATFORM_VERSIONS+=("$VERSION")
+  docker buildx build -t ${REPO}/${IMAGE}:${VERSION}_${sanitised_p} --compress --push --platform "linux/$p" .
+done
+
+# Check to see that we have a common version between all platforms
+for v in "${PLATFORM_VERSIONS[@]}"; do
+  if [[ "$v" != "${PLATFORM_VERSIONS[0]}" ]]; do
+    echo "WARNING: Cannot create multi-arch image, as versions are different between architectures."
+    echo "${PLATFORM_VERSIONS[@]}"
+    exit 0
+  fi
+done
 
 # Build & push latest
 docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform "${PLATFORMS}" .
